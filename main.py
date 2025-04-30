14196e576980402b3d1d5817b0892b0c8d372cf7
Jeff Hobson <jeffrey296@gmail.com>
2025-04-29 22:37:53 -0400

    Simplify project from Cloud Run service to function
    This is always what it was intended to be

---
+++ main.py
@@ -0,0 +1,27 @@
+import functions_framework
+from kh2rando.seed_gen import generator
+
+@functions_framework.http
+def respond_to_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'preset' in request_json:
+        preset = request_json['preset']
+    elif request_args and 'preset' in request_args:
+        preset = request_args['preset']
+    else:
+        preset = 'League Summer 2025'
+    
+    # TODO: Add some error handling here
+    seed_info = generator.make_random_seed_from_preset_name(preset)
+    return seed_info.generator_string
