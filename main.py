30d09045a079f9672a5a14e81f8574447250e46b
Jeff Hobson <jeffrey296@gmail.com>
2025-04-30 12:05:28 -0400

    Try removing some more arguments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import functions_framework
 from kh2rando.seed_gen import generator
 
 @functions_framework.http
-def kh2-rando-seedgen(request):
+def kh2_rando_seedgen(request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
